546d779dc2c7b9be7c1256f715253b0efe0afb3e
youpie <youplamb@hotmail.nl>
2024-09-04 17:28:48 +0200

    Initial attempt. But will change to using geckodriver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use std::error::Error;
+
+async fn send_request() -> Result<String, Box<dyn Error>> {
+    Ok(
+        reqwest::get("https://webcom.connexxion.nl/WebCommWebBalancer")
+            .await?
+            .text()
+            .await?,
+    )
+}
+
+#[tokio::main]
+async fn main() {
+    let response = send_request().await;
+    println!("{}", scraper::Html::parse_document(&response.unwrap()));
 }

7dc5f10f360a90de2eb43d15c3066c51756d4003
youpie <youplamb@hotmail.nl>
2025-08-18 22:45:35 +0200

    added mongo branch

--- migrations/20250818180351_donation_text.up.sql
+++
@@ -1,9 +0,0 @@
--- Add up migration script here
-CREATE TABLE donation_text (
-    id SERIAL PRIMARY KEY,
-    donate_link TEXT NOT NULL,
-    donate_text TEXT NOT NULL,
-    donate_service_name TEXT NOT NULL,
-    iban TEXT NOT NULL,
-    iban_name TEXT NOT NULL
-);
--- migrations/20250818180545_email_properties.up.sql
+++
@@ -1,9 +0,0 @@
--- Add up migration script here
-CREATE TABLE email_properties (
-    id SERIAL PRIMARY KEY,
-    smtp_server TEXT NOT NULL,
-    smtp_username TEXT NOT NULL,
-    smtp_password TEXT NOT NULL,
-    mail_from TEXT NOT NULL,
-    support_mail TEXT NOT NULL
-);

1f92157d7c069df9a3be5e5a0ab90bc0c32a6f3c
youpie <youplamb@hotmail.nl>
2025-10-14 16:41:01 +0200

    added debug print

--- src/variables.rs
+++ src/variables.rs
@@ -9,6 +9,7 @@ use thirtyfour::WebDriver;
 
 // I understand this might be an unconventional approach, but I really like this workflow so if I could get this working I would be really happy
 // Thank you for taking your time looking at my issue btw!
+// The debug print of this is printed below the struct
 
 #[allow(dead_code)]
 #[derive(DerivePartialModel, Debug)]
@@ -27,6 +28,8 @@ pub struct GeneralProperties {
     password_reset_link: String,
     #[sea_orm(nested)]
     kuma_properties: KumaPropertiesA, // The general properties contains a kuma_properties nested relation
+    #[sea_orm(from_col = "general_email_properties")]
+    email_id: i32,
     #[sea_orm(nested)]
     general_email_properties: email_properties::Model, // Both the general properties and kuma properties have a email properties relation, but to a different ID of email properties
     #[sea_orm(nested)]
@@ -50,6 +53,8 @@ pub struct KumaPropertiesA {
     domain: String,
     hearbeat_retry: i32,
     offline_mail_resend_hours: i32,
+    #[sea_orm(from_col = "kuma_email_properties")]
+    email_id: i32,
     #[sea_orm(nested)]
     kuma_email_properties: email_properties::Model, // The kuma needs to send mails from a different SMTP server
     mail_port: i32,
@@ -66,6 +71,52 @@ struct DonationTextA {
     iban: String,
     iban_name: String,
 }
+
+/*
+GeneralProperties {
+    save_target: "./calendar/",
+    ical_domain: "calendar.bussie.app",
+    webcal_domain: "webcal.bussie.app",
+    pdf_shift_domain: "shift.bussie.app",
+    signin_fail_execution_reduce: 2,
+    signin_fail_mail_reduce: 4,
+    execution_interval_minutes: 3600,
+    expected_execution_time_seconds: 20,
+    execution_retry_count: 8,
+    support_mail: "support@bussie.app",
+    password_reset_link: "link.bussie.app",
+    kuma_properties: KumaPropertiesA {
+        domain: "stats.emphisia.nl",
+        hearbeat_retry: 1,
+        offline_mail_resend_hours: 1,
+        email_id: 2,
+        kuma_email_properties: Model {
+            email_id: 1,
+            mail_from: "youplamb@hotmail.nl",
+            smtp_server: "mail.emphisia.nl",
+            smtp_username: "nextcloud@emphisia.nl",
+            smtp_password: "123qwerty",
+        },
+        mail_port: 465,
+        use_ssl: true,
+    },
+    email_id: 1,
+    general_email_properties: Model {
+        email_id: 1,
+        mail_from: "youplamb@hotmail.nl",
+        smtp_server: "mail.emphisia.nl",
+        smtp_username: "nextcloud@emphisia.nl",
+        smtp_password: "123qwerty",
+    },
+    donation_text: DonationTextA {
+        donate_link: "google.com",
+        donate_text: "Thanx",
+        donate_service_name: "google",
+        iban: "NL1234",
+        iban_name: "Youp",
+    },
+}
+ */
 
 #[allow(dead_code)]
 struct UserData {

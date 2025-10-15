c62c1e4e57d8a7b40e9dbb6411583db403039c5c
youpie <youplamb@hotmail.nl>
2025-10-15 13:56:02 +0200

    first implementation of multiple nested structs

--- src/database.rs
+++ src/database.rs
@@ -1,19 +1,26 @@
 use entity::{
-    donation_text, email_properties, kuma_properties,
+    donation_text, email_properties, general_properties_db, kuma_properties,
     prelude::{DonationText, EmailProperties, GeneralPropertiesDb, KumaProperties},
 };
-use sea_orm::{Database, EntityTrait};
+use migration::Alias;
+use sea_orm::{Database, EntityTrait, QuerySelect, RelationTrait};
 
-use crate::variables::GeneralProperties;
+use crate::variables::{GeneralProperties, KumaPropertiesA};
 
 pub async fn get_kuma_email() {
     let db = Database::connect("postgres://postgres:123qwerty@localhost/postgres")
         .await
         .unwrap();
+
     let properties: GeneralProperties = GeneralPropertiesDb::find_by_id(1)
         .left_join(kuma_properties::Entity)
         .left_join(email_properties::Entity)
         .left_join(donation_text::Entity)
+        .join_as(
+            sea_orm::JoinType::LeftJoin,
+            kuma_properties::Relation::EmailProperties.def(),
+            "kuma_email",
+        )
         .into_partial_model()
         .one(&db)
         .await
--- src/variables.rs
+++ src/variables.rs
@@ -3,14 +3,6 @@ use entity::{donation_text, email_properties, general_properties_db, kuma_proper
 use sea_orm::{DerivePartialModel, FromQueryResult};
 use thirtyfour::WebDriver;
 
-// If you are reading this i'll try to explain it
-// The Foreign keys to the email properties are created in migration/src/m20251006_141509_kuma.rs
-// And migration/src/m20251006_143409_general_settings.rs
-
-// I understand this might be an unconventional approach, but I really like this workflow so if I could get this working I would be really happy
-// Thank you for taking your time looking at my issue btw!
-// The debug print of this is printed below the struct
-
 #[allow(dead_code)]
 #[derive(DerivePartialModel, Debug)]
 #[sea_orm(entity = "GeneralPropertiesDb")]
@@ -27,11 +19,9 @@ pub struct GeneralProperties {
     support_mail: String,
     password_reset_link: String,
     #[sea_orm(nested)]
-    kuma_properties: KumaPropertiesA, // The general properties contains a kuma_properties nested relation
-    #[sea_orm(from_col = "general_email_properties")]
-    email_id: i32,
+    kuma_properties: KumaPropertiesA,
     #[sea_orm(nested)]
-    general_email_properties: email_properties::Model, // Both the general properties and kuma properties have a email properties relation, but to a different ID of email properties
+    general_email_properties: EmailPropertiesA,
     #[sea_orm(nested)]
     donation_text: DonationTextA,
 }
@@ -55,8 +45,8 @@ pub struct KumaPropertiesA {
     offline_mail_resend_hours: i32,
     #[sea_orm(from_col = "kuma_email_properties")]
     email_id: i32,
-    #[sea_orm(nested)]
-    kuma_email_properties: email_properties::Model, // The kuma needs to send mails from a different SMTP server
+    #[sea_orm(nested, alias = "kuma_email")]
+    kuma_email_properties: email_properties::Model,
     mail_port: i32,
     use_ssl: bool,
 }
@@ -71,52 +61,6 @@ struct DonationTextA {
     iban: String,
     iban_name: String,
 }
-
-/*
-GeneralProperties {
-    save_target: "./calendar/",
-    ical_domain: "calendar.bussie.app",
-    webcal_domain: "webcal.bussie.app",
-    pdf_shift_domain: "shift.bussie.app",
-    signin_fail_execution_reduce: 2,
-    signin_fail_mail_reduce: 4,
-    execution_interval_minutes: 3600,
-    expected_execution_time_seconds: 20,
-    execution_retry_count: 8,
-    support_mail: "support@bussie.app",
-    password_reset_link: "link.bussie.app",
-    kuma_properties: KumaPropertiesA {
-        domain: "stats.emphisia.nl",
-        hearbeat_retry: 1,
-        offline_mail_resend_hours: 1,
-        email_id: 2,
-        kuma_email_properties: Model {
-            email_id: 1,
-            mail_from: "youplamb@hotmail.nl",
-            smtp_server: "mail.emphisia.nl",
-            smtp_username: "nextcloud@emphisia.nl",
-            smtp_password: "123qwerty",
-        },
-        mail_port: 465,
-        use_ssl: true,
-    },
-    email_id: 1,
-    general_email_properties: Model {
-        email_id: 1,
-        mail_from: "youplamb@hotmail.nl",
-        smtp_server: "mail.emphisia.nl",
-        smtp_username: "nextcloud@emphisia.nl",
-        smtp_password: "123qwerty",
-    },
-    donation_text: DonationTextA {
-        donate_link: "google.com",
-        donate_text: "Thanx",
-        donate_service_name: "google",
-        iban: "NL1234",
-        iban_name: "Youp",
-    },
-}
- */
 
 #[allow(dead_code)]
 struct UserData {
